99c9199322e676fd08944b39bac48b551bb97860
hisayuki <syuaibsyuaib@gmail.com>
2021-08-30 01:50:12 +0800

    fix curl

--- proses.php
+++ proses.php
@@ -1,40 +1,48 @@
 <?php
-// if (isset($_POST['masuk'])) {
-//     $pengguna = $_POST['pengguna'];
-//     $sandi = $_POST['sandi'];
-//     if ($pengguna == "admin" && $sandi == "admin") {
-//         header("location: admin.html");
-//     } elseif ($pengguna == "piket" && $sandi == "piket") {
-//         header("location: tamu.html");
-//     } else {
-//         header("location: index.html");
-//     }
-// }
 
 if (isset($_POST['masuk'])) {
     $pengguna = $_POST['pengguna'];
     $sandi = $_POST['sandi'];
-
     $url = "https://script.google.com/macros/s/AKfycbx6QxaoEdDJf8e9zItLDwD6Oq6er4L8cnknO2ET2E-mBxK2QqM/exec";
-    $data = json_encode(array("pengguna" => $pengguna, "sandi" => $sandi));
-    $options = array(
-        "http" => array(
-            "method" => "POST",
-            "header" => "Content-Type: application/json",
-            "content" => $data
-        )
-    );
-    // $context = stream_context_create($options)
-    $result = file_get_contents($url, false, stream_context_create($options));
-    $res = json_decode($result, true);
-    if($res){
-        session_start();
-        $_SESSION['user'] = $res["pengguna"];
-        $_SESSION['pass'] = $res["sandi"];
-        $_SESSION['role'] = $res["role"];
-    };
-    echo $res["pengguna"] . "<br>";
-    echo $res["sandi"];
-}
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-?>
+    $headers = array(
+        "Content-Type: application/json",
+    );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    $data = json_encode(array("pengguna" => $pengguna, "sandi" => $sandi));
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+    //for debug only!
+    // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    $resp = curl_exec($curl);
+    curl_close($curl);
+    $hasil = json_decode($resp, true);
+    
+    if ($hasil) {
+        session_start();
+        $_SESSION['user'] = $pengguna;
+        $_SESSION['pass'] = $sandi;
+        $_SESSION['role'] = $hasil["role"];
+        
+        if ($hasil["role"] == 1) {
+            header("location: admin.html");
+        } elseif ($hasil["role"] == 2) {
+            header("location: tamu.html");
+        } elseif ($hasil["role"] == 3) {
+            header("location: pejabat.html");
+        } else {
+            header("location: index.html");
+        }
+    };
+
+    
+}
