0c0fb410324c03325a8d99ccffeb98a3488bcb71
hisayuki <syuaibsyuaib@gmail.com>
2021-09-22 19:17:06 +0800

    fix: fungsi tanya_simpan(global function

--- syuaib.php
+++ syuaib.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-    $arr = ["Nol", "Satu", "Dua", "Tiga", "Empat", "Lima"];
-    print_r($arr);
-    echo "<br>";
-    print_r(array_slice($arr,3));
-    ?>
-    <script>
-        let isi = [1,2,3];
-
-        <?php
-        // echo 'isi';
-         echo 'isi';
-        // var_dump($isi);
-         
-         ?>
-    </script>
-</body>
-
-</html>
+<?php
+phpinfo();
+?>
