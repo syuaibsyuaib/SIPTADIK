7b264a9dc14eee7ca41ae4bb96212d7296cd0a28
Muh. Hadi Purnomo <nuzumaki247@gmail.com>
2021-09-24 17:55:12 +0800

    Tidak ada apa-apa

--- syuaib.php
+++ syuaib.php
@@ -1,3 +1,6 @@
 <?php
-phpinfo();
+// date_default_timezone_set("Asia/Makassar");
+setlocale(LC_ALL, 'id_ID');
+// setlocale(LC_ALL, 'da_DA');
+echo date("d/m/Y h:i", strtotime("20-01-2021 15:20"));
 ?>
