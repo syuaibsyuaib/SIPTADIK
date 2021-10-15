6edb2e5002017f00c4c2579dc18c0de53a79d848
Muh. Hadi Purnomo <nuzumaki247@gmail.com>
2021-10-16 02:53:34 +0800

    Modifikasi notif

--- layout/header.php
+++ layout/header.php
@@ -306,9 +306,9 @@ $dataBidang = $_SESSION['data']['dataBidang'];
     <!-- NOTIFIKASI -->
     <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 1080">
         <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header bg-success text-white">
+            <div class="toast-header">
                 <strong class="me-auto">SIPTADIK</strong>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
                 <div id="pesanNotif"></div>
